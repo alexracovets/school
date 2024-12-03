fd4b649ebef76650cdd32e7fa0b89d9b09a26add
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-12-03 03:46:56 +0200

    added burger

--- shared/components/shared/headerNavigation.tsx
+++ shared/components/shared/headerNavigation.tsx
@@ -6,6 +6,7 @@ import { Drawer, DrawerContent, DrawerTitle, DrawerHeader, DrawerDescription, Se
 import useResponsive from '@/store/useResponsive';
 import useNavMenu from '@/store/useNavMenu';
 import { cn } from "@/shared/lib";
+import { useEffect } from "react";
 
 export const HeaderNavigation = () => {
     const isOpen = useNavMenu(state => state.isOpen);
@@ -36,60 +37,64 @@ export const HeaderNavigation = () => {
             to: "#start"
         }
     ]
+
+    useEffect(() => {
+        if (!isMobile && !isTablet) {
+            setIsOpen(false);
+        }
+    }, [isMobile, isTablet, setIsOpen])
     return (
         <>
-            {isTablet || isMobile ?
-                <Drawer open={isOpen} onOpenChange={setIsOpen} direction='right'>
-                    <DrawerContent>
-                        <DrawerHeader className='hidden'>
-                            <DrawerTitle></DrawerTitle>
-                            <DrawerDescription></DrawerDescription>
-                        </DrawerHeader>
-                        <nav
-                            style={{ marginTop: headerHeight + "px" }}
-                            className={cn(
-                                "pointer-events-auto h-full",
-                                "max-mobile:w-full"
-                            )}>
-                            <ul className={cn(
-                                "flex flex-col min-w-[50vw] pt-[3rem]",
-                                "max-mobile:min-w-[100vw] max-mobile:w-full"
-                            )}>
-                                {links.map((item, idx) => {
-                                    return (
-                                        <li key={idx} className="w-full">
-                                            <Link
-                                                href={item.path}
-                                                className={cn(
-                                                    "flex text-[1.6rem] font-kyiv_titling leading-1 px-[1.6rem] py-[1.5rem] bg-regal-light-blue-fourd w-full"
-                                                )}
-                                            >
-                                                {item.name}
-                                            </Link>
-                                            <Separator />
-                                        </li>
-                                    )
-                                })
-                                }
-                            </ul>
-                        </nav>
-                    </DrawerContent>
-                </Drawer> :
-                <nav>
-                    <ul className="flex text-[3.058rem] font-[400] font-kyiv_titling gap-[10rem]">
-                        {links.map((item, idx) => {
-                            return (
-                                <li key={idx}>
-                                    <Link href={item.path}>
-                                        {item.name}
-                                    </Link>
-                                </li>
-                            )
-                        })
-                        }
-                    </ul>
-                </nav>
-            }
+            <Drawer open={isOpen} onOpenChange={setIsOpen} direction='right'>
+                <DrawerContent>
+                    <DrawerHeader className='hidden'>
+                        <DrawerTitle></DrawerTitle>
+                        <DrawerDescription></DrawerDescription>
+                    </DrawerHeader>
+                    <nav
+                        style={{ marginTop: headerHeight + "px" }}
+                        className={cn(
+                            "pointer-events-auto h-full",
+                            "max-mobile:w-full"
+                        )}>
+                        <ul className={cn(
+                            "flex flex-col min-w-[50vw] pt-[3rem]",
+                            "max-mobile:min-w-[100vw] max-mobile:w-full"
+                        )}>
+                            {links.map((item, idx) => {
+                                return (
+                                    <li key={idx} className="w-full">
+                                        <Link
+                                            href={item.path}
+                                            className={cn(
+                                                "flex text-[1.6rem] font-kyiv_titling leading-1 px-[1.6rem] py-[1.5rem] bg-regal-light-blue-fourd w-full"
+                                            )}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                        <Separator />
+                                    </li>
+                                )
+                            })
+                            }
+                        </ul>
+                    </nav>
+                </DrawerContent>
+            </Drawer>
+            <nav className="max-tablet:hidden">
+                <ul className="flex text-[3.058rem] font-[400] font-kyiv_titling gap-[10rem]">
+                    {links.map((item, idx) => {
+                        return (
+                            <li key={idx}>
+                                <Link href={item.path}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+            </nav>
         </>
     )
 }
